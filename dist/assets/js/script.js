4eb142663746337b960fe80e300dc18d5e920a21
unknown <pakelvi0707@gmail.com>
2021-11-09 17:03:05 +0300

    section bussines, step, company

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -17,5 +17,7 @@ new Swiper ('.about-store__right-block');
 
 
 new Swiper ('.format__slide');
+new Swiper ('.plan__slider');
+new Swiper ('.step__slider');
 
 //# sourceMappingURL=script.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,3 +3,5 @@ new Swiper ('.about-store__right-block');
 
 
 new Swiper ('.format__slide');
+new Swiper ('.plan__slider');
+new Swiper ('.step__slider');
