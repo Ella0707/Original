f02bc36246c56227dc1da8ce40c403d753d38816
unknown <pakelvi0707@gmail.com>
2021-12-15 17:04:02 +0300

    svg map

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -271,4 +271,103 @@ vid.addEventListener('mouseenter', function() {
 vid.addEventListener('mouseleave', function() {
     vid.pause();
 })
+
+
+
+// map
+
+$('.point-link').hover (
+    function() {
+        $('.description').html($(this).attr('description-data'));
+        $('.description').fadeIn(50);
+        $('.description').style.top = (e.y);
+        $('.description').style.left = (e.x);
+
+        let coords = target.getBoundingClientRect();
+
+        let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+        if (left < 0) left = 0; // не заезжать за левый край окна
+  
+        let top = coords.top - tooltipElem.offsetHeight - 5;
+        if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+          top = coords.top + target.offsetHeight + 5;
+        }
+  
+        tooltipElem.style.left = left + 'px';
+        tooltipElem.style.top = top + 'px';
+
+    },
+    function() {
+        $('.description').fadeOut(50);
+    }
+)
+
+// const description = document.querySelector('.description');
+// const point = document.querySelector('.point-link');
+
+// point.forEach(point => {
+//     point.addEventListener('mosemove', function(e) {
+//         description.innerText = this.dataset.title;
+//         point.style.top = (e.y + 20) + 'px';
+//         point.style.top = (e.x + 20) + 'px';
+//     });
+// });
+
+
+//  let tooltipElem;
+
+//     document.onmouseover = function(event) {
+//       let target = event.target;
+
+//       // если у нас есть подсказка...
+//       let tooltipHtml = target.dataset.tooltip;
+//       if (!tooltipHtml) return;
+
+//       // ...создадим элемент для подсказки
+
+//       tooltipElem = document.createElement('div');
+//       tooltipElem.className = 'tooltip';
+//       tooltipElem.innerHTML = tooltipHtml;
+//       document.body.append(tooltipElem);
+
+//       // спозиционируем его сверху от аннотируемого элемента (top-center)
+//       let coords = target.getBoundingClientRect();
+
+//       let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+//       if (left < 0) left = 0; // не заезжать за левый край окна
+
+//       let top = coords.top - tooltipElem.offsetHeight - 5;
+//       if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+//         top = coords.top + target.offsetHeight + 5;
+//       }
+
+//       tooltipElem.style.left = left + 'px';
+//       tooltipElem.style.top = top + 'px';
+//     };
+
+//     document.onmouseout = function(e) {
+
+//       if (tooltipElem) {
+//         tooltipElem.remove();
+//         tooltipElem = null;
+//       }
+
+//     };
+
+
+
+
+
+		$(document).ready(function(){//страница загрузилась
+			$('.point-link').hover(function(){	//при наведении на элемент:
+				var helptext = $(this).attr('description-data') 	//текст подсказки
+				$('.description').html(helptext).show();	//размещаем текст подсказки в блок тултипа и делаем его видимым
+				//далее устанавливаем тултипу значения позиции с помощью абсолютного позиционирования:
+				$('.description').offset({top:$(this).offset(0).top+$(this).height(0),left:$(this).offset(0).left});
+			},function(){
+				$('.description').hide();	//скрываем тултип 
+			});
+		});
+		//в итоге подсказка окажется ровно под элементом наведения
+	
 //# sourceMappingURL=script.js.map
