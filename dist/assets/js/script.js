4416edfc54e22c3111ea7e9d38b40a10cf5b10db
Ella0707 <pakelvi77@gmail.com>
2021-12-20 00:02:23 +0300

    smooth scrolling

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -287,5 +287,18 @@ $(".point").hover(
 
 
 
+// плавная прокрутка
 
+let anchors = document.querySelectorAll('a[href*="#"]');
+
+for(let anchor of anchors ) {
+    anchor.addEventListener('click', function(event) {
+    event.preventDefault()
+    let blockID = anchor.getAttribute('href')
+    document.querySelector(blockID).scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+    })
+    })
+    }
 //# sourceMappingURL=script.js.map
